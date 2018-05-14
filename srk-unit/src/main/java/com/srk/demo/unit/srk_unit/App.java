948a213a551d2e6d492686243ae14d0aa36094c5
Senthil Kumar <senthil@senthils-mbp.lan>
2018-05-14 16:37:55 +0200

    Json parser added

--- srk-unit/src/main/java/com/srk/demo/unit/srk_unit/App.java
+++ srk-unit/src/main/java/com/srk/demo/unit/srk_unit/App.java
@@ -1,13 +1,21 @@
 package com.srk.demo.unit.srk_unit;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 /**
- * Hello world!
+ * Json Parsing
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World Java Program!" );
-    }
+public class App {
+	public static void main(String[] args) {
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		Device device = new Device(100);
+		try {
+			System.out.println(objectMapper.writeValueAsString(device));
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+	}
 }
