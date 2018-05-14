5efd845175b978c204300e7dbde6b797d2703808
Senthil Kumar <senthil@senthils-mbp.lan>
2018-05-14 15:55:20 +0200

    Ïnitial Version

---
+++ srk-unit/src/main/java/com/srk/demo/unit/srk_unit/App.java
@@ -0,0 +1,13 @@
+package com.srk.demo.unit.srk_unit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
