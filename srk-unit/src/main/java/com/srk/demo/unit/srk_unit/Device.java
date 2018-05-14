f9eb53ba19882e49d3c918818b8acfca6d45e4d4
Senthil Kumar <senthil@senthils-mbp.lan>
2018-05-14 16:11:05 +0200

    Unit test case added

---
+++ srk-unit/src/main/java/com/srk/demo/unit/srk_unit/Device.java
@@ -0,0 +1,16 @@
+package com.srk.demo.unit.srk_unit;
+
+public class Device {
+	
+	private int deviceID = -1;
+	
+	public  Device(int deviceID)
+	{
+		this.deviceID = deviceID;
+	}
+
+	public int getID() {
+		return deviceID;
+	}
+
+}
--- srk-unit/src/test/java/com/srk/demo/unit/srk_unit/AppTest.java
+++ srk-unit/src/test/java/com/srk/demo/unit/srk_unit/AppTest.java
@@ -1,5 +1,9 @@
 package com.srk.demo.unit.srk_unit;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -7,32 +11,35 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest extends TestCase {
+	/**
+	 * Create the test case
+	 *
+	 * @param testName
+	 *            name of the test case
+	 */
+	public AppTest(String testName) {
+		super(testName);
+	}
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(AppTest.class);
+	}
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	/**
+	 * Rigourous Test :-)
+	 */
+	public void testApp1() {
+		Device device = new Device(1);
+		assertEquals("Device ID mismatch ", 1, device.getID());
+	}
+
+	public void testApp2() {
+		Device device = mock(Device.class);
+		when(device.getID()).thenReturn(2);
+		assertEquals("Device ID mismatch ", 2, device.getID());
+	}
 }
